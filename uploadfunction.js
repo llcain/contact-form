c0015161c567822ff585d3c1ddfad4b15d5fedcd
llcain <llcain2012@gmail.com>
2024-04-07 21:07:29 -0500

    Added jquery

--- uploadfunction.js
+++ uploadfunction.js
@@ -1,12 +1,3 @@
 $(document).ready(function() {
-    $('#first_form').submit(function(e) {
-        e.preventDefault();
-        var name = $('#name').val();
-        var email = $('#email').val();
-        var message = $('#message').val();
-
-        if(name, email, message === "") {
-            
-        }
-    })
-})
+    $("#first_form").validate();
+});
