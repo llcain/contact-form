126da3dbecc34ac85d70a7166a174a7a5611b449
llcain <llcain2012@gmail.com>
2024-04-07 20:48:39 -0500

    Created JS file

---
+++ uploadfunction.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('#first_form').submit(function(e) {
+        e.preventDefault();
+        var name = $('#name').val();
+        var email = $('#email').val();
+        var message = $('#message').val();
+
+        if(name, email, message === "") {
+            
+        }
+    })
+})
